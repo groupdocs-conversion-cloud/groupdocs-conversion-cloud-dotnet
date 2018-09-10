aa749377160895061bd5bcc62992686404ed1675
Muhammad Rizwan Niazi <muhammad.rizwan@groupdocs.com>
2018-09-10 19:32:31 +0500

    Added SDK Examples

---
+++ GroupDocs.Conversion.Cloud.Examples/Common.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupDocs.Conversion.Cloud.Examples
+{
+    class Common
+    {
+        public static string MyAppSid = Common.MyAppSid;
+        public static string MyAppKey = Common.MyAppKey;
+    }
+}
