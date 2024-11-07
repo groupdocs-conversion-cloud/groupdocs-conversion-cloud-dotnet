66af1f9d45469f96d9251e77beb22ce7769346c6
product-team <product.team@groupdocs.com>
2024-11-07 01:42:35 -0600

    Updated sources

--- src/GroupDocs.Conversion.Cloud.Sdk/Model/WebLoadOptions.cs
+++ src/GroupDocs.Conversion.Cloud.Sdk/Model/WebLoadOptions.cs
@@ -39,9 +39,42 @@ namespace GroupDocs.Conversion.Cloud.Sdk.Model
     public class WebLoadOptions : LoadOptions 
     {                       
         /// <summary>
+        /// Controls how HTML content is rendered. Default: AbsolutePositioning
+        /// </summary>
+        /// <value>Controls how HTML content is rendered. Default: AbsolutePositioning</value>
+        [JsonConverter(typeof(StringEnumConverter))]
+        public enum RenderingModeEnum
+        { 
+            /// <summary>
+            /// Enum Flow for "Flow"
+            /// </summary>            
+            Flow,
+            
+            /// <summary>
+            /// Enum AbsolutePositioning for "AbsolutePositioning"
+            /// </summary>            
+            AbsolutePositioning            
+        }
+
+        /// <summary>
+        /// Controls how HTML content is rendered. Default: AbsolutePositioning
+        /// </summary>
+        public RenderingModeEnum? RenderingMode { get; set; }
+
+        /// <summary>
         /// Enable or disable generation of page numbering in converted document. Default: false
         /// </summary>  
         public bool? PageNumbering { get; set; }
+
+        /// <summary>
+        /// Get or sets the encoding to be used when loading the web document. If the property is null the encoding will be determined from document character set attribute
+        /// </summary>  
+        public string Encoding { get; set; }
+
+        /// <summary>
+        /// Use pdf for the conversion. Default: false
+        /// </summary>  
+        public bool? UsePdf { get; set; }
 
         /// <summary>
         /// Get the string presentation of the object
@@ -52,6 +85,9 @@ namespace GroupDocs.Conversion.Cloud.Sdk.Model
           var sb = new StringBuilder();
           sb.Append("class WebLoadOptions {\n");
           sb.Append("  PageNumbering: ").Append(this.PageNumbering).Append("\n");
+          sb.Append("  Encoding: ").Append(this.Encoding).Append("\n");
+          sb.Append("  UsePdf: ").Append(this.UsePdf).Append("\n");
+          sb.Append("  RenderingMode: ").Append(this.RenderingMode).Append("\n");
           sb.Append("}\n");
           return sb.ToString();
         }
